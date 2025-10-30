e4aa51dc4d122536c37e1764694d952a8f6b7be3
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-10-31 06:57:59 +0700

    add clerk, neon, and prisma connections

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import {clerkMiddleware} from '@clerk/express';
 
 dotenv.config();
 
@@ -9,12 +10,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
 app.use(cors());
 
+app.use(clerkMiddleware());
+
 app.get('/', (_, res) => {
-    res.send('Project Management Backend is running');
+  res.send('Project Management Backend is running');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
