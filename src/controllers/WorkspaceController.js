54e732c4554e784c6cb56d17f9f0c66decf0c02c
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-11-04 07:22:54 +0700

    feat: implement add member to workspace handler

--- src/controllers/WorkspaceController.js
+++ src/controllers/WorkspaceController.js
@@ -31,3 +31,64 @@ export const GetUserWorkspaces = async (request, response) => {
     return response.status(500).json({error: 'Internal Server Error'});
   }
 };
+
+export const AddMemberToWorkspace = async (request, response) => {
+  try {
+    const {userId} = request.auth;
+
+    const {email, role, workspaceId, message} = request.body;
+
+    const user = await prisma.user.findUnique({
+      where: {email: email},
+    });
+
+    if (!user) {
+      return response.status(404).json({error: 'User not found'});
+    }
+
+    if (!workspaceId || !role) {
+      return response.status(400).json({error: 'Missing workspaceId or role'});
+    }
+
+    if (!['ADMIN', 'MEMBER'].includes(role)) {
+      return response.status(400).json({error: 'Invalid role specified'});
+    }
+
+    const workspace = await prisma.workspace.findUnique({
+      where: {id: workspaceId},
+      include: {members: true},
+    });
+
+    if (!workspace) {
+      return response.status(404).json({error: 'Workspace not found'});
+    }
+
+    if(!workspace.members.find((member) => member.userId === userId && member.role === 'ADMIN')) {
+      return response.status(403).json({error: 'Only admins can add members to the workspace'});
+    }
+
+    const existingMember = workspace.members.find(
+      (member) => member.userId === user.id
+    );
+
+    if (existingMember) {
+      return response.status(400).json({error: 'User is already a member of the workspace'});
+    }
+
+    const newMember = await prisma.workspaceMember.create({
+      data: {
+        workspaceId: workspaceId,
+        userId: user.id,
+        role: role,
+        addedBy: userId,
+        message: message || null,
+      },
+    });
+
+    return response.status(201).json({newMember, message: 'Member added successfully'});
+
+  } catch (error) {
+    console.error('Error adding member to workspace:', error);
+    return response.status(500).json({error: 'Internal Server Error'});
+  }
+};
