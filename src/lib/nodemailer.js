15d6ff65f3eeedcdecc414af8d776b6e0c641ea8
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-11-23 08:07:57 +0700

    fix: update nodemailer transport host for email sending

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -1,9 +1,8 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: "brevo",
+    host: "smtp-relay.brevo.com",
     port: 587,
-    secure: false, 
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
@@ -14,7 +13,7 @@ const SendEmail = async ({ to, subject, body }) => {
     const response = await transporter.sendMail({
         from: process.env.EMAIL_SENDER,
         to,
-        subject, 
+        subject,
         html: body,
     });
 
